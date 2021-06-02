d2cc6eb0920ec6c4f22f333202b27a3a6fbf61c0
KKozhinov <84965550+firsov2211@users.noreply.github.com>
2021-06-02 11:37:57 +0300

    first commit

---
+++ Roddom/Roddom/AddWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Roddom
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public Product CurrentProduct { get; set; }
+        public IEnumerable<ProductType> ProductTypes { get; set; }
+
+
+        public AddWindow(Product products)
+        {
+            InitializeComponent();
+            DataContext = this;
+            CurrentProduct = products;
+            ProductTypes = Core.DB.ProductType.ToArray();
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (CurrentProduct.ID == 0)
+                    Core.DB.Product.Add(CurrentProduct);
+
+                Core.DB.SaveChanges();
+
+                DialogResult = true;
+
+                MessageBox.Show($"Успешно!");
+            }
+            catch
+            {
+                MessageBox.Show($"Error" +
+                    $"Проверьте введённые данные");
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Roddom/Roddom/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roddom
+{
+    class Core
+    {
+        public static vfirsovEntities DB = new vfirsovEntities();
+    }
+}
